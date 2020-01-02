2d72848011cc38924b37b5fd503a3e19f09bb5a0
Matias Insaurralde <matias@insaurral.de>
2020-01-02 04:13:49 +0000

    Remove wasm-bindgen references

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -45,7 +45,6 @@ use crate::{
     realm::Realm,
     syntax::{ast::expr::Expr, lexer::Lexer, parser::Parser},
 };
-use wasm_bindgen::prelude::*;
 
 fn parser_expr(src: &str) -> Expr {
     let mut lexer = Lexer::new(src);
@@ -82,46 +81,4 @@ pub fn exec(src: &str) -> String {
     let realm = Realm::create();
     let mut engine: Interpreter = Executor::new(realm);
     forward(&mut engine, src)
-}
-
-// WASM
-#[wasm_bindgen]
-extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn evaluate(src: &str) -> String {
-    let mut lexer = Lexer::new(&src);
-    match lexer.lex() {
-        Ok(_v) => (),
-        Err(v) => log(&v.to_string()),
-    }
-
-    let tokens = lexer.tokens;
-
-    // Setup executor
-    let expr: Expr;
-
-    match Parser::new(tokens).parse_all() {
-        Ok(v) => {
-            expr = v;
-        }
-        Err(_v) => {
-            log("parsing fail");
-            return String::from("parsing failed");
-        }
-    }
-    // Create new Realm
-    let realm = Realm::create();
-    let mut engine: Interpreter = Executor::new(realm);
-    let result = engine.run(&expr);
-    log("test2");
-    match result {
-        Ok(v) => v.to_string(),
-        Err(v) => format!("{}: {}", "error", v.to_string()),
-    }
 }
